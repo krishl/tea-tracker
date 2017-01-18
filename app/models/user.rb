aa6abec3533912699f52db4292f48c983d47fe8a
Krishel Lasam <krishel.lasam@gmail.com>
2017-01-18 03:27:54 -0500

    add users controller, slug users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,14 @@
 class User < ActiveRecord::Base
   has_secure_password
   has_many :teas
+
+  def slug
+    username.chomp.downcase.gsub(" ", "-")
+  end
+
+  def self.find_by_slug(slug)
+    User.all.find do |user|
+      user.slug == slug
+    end
+  end
 end

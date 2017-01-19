7e4457b65903013d6b732edf975b1108e6192898
Krishel Lasam <krishel.lasam@gmail.com>
2017-01-19 05:41:41 -0500

    add delete tea

--- app/controllers/teas_controller.rb
+++ app/controllers/teas_controller.rb
@@ -11,6 +11,7 @@ class TeasController < ApplicationController
 
   get '/teas/new' do
     if logged_in?
+      @user = current_user
       erb :'teas/create_tea'
     else
       redirect to "/login"
@@ -51,5 +52,22 @@ class TeasController < ApplicationController
     @tea = Tea.find_by_id(params[:id])
     @tea.update(kind: params[:kind], purchase_date: params[:purchase_date], brew_time: params[:brew_time], temperature: params[:temperature], grams: params[:grams], servings: params[:servings], name: params[:name])
     redirect to "/teas/#{@tea.id}"
+  end
+
+  delete '/teas/:id/delete' do
+    if logged_in?
+      @tea = Tea.find_by_id(params[:id])
+      if @tea.user_id == current_user.id
+        @tea.destroy
+        flash[:message] = "Successfully deleted tea entry!"
+        redirect to "/teas"
+      else
+        flash[:message] = "Cannot delete another user's tea entry."
+        redirect to "/teas"
+      end
+    else
+      flash[:message] = "You are not logged in."
+      redirect to "/login"
+    end
   end
 end

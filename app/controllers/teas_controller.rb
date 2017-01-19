b7fc46a91298fa197338ec00bba0fa0361a7bcc3
Krishel Lasam <krishel.lasam@gmail.com>
2017-01-19 04:52:25 -0500

    add edit page

--- app/controllers/teas_controller.rb
+++ app/controllers/teas_controller.rb
@@ -1,22 +1,49 @@
 class TeasController < ApplicationController
   get '/teas' do
-    @user = current_user
-    @teas = Tea.all
-    erb :'teas/teas'
+    if logged_in?
+      @user = current_user
+      @teas = Tea.all
+      erb :'teas/teas'
+    else
+      redirect to "/login"
+    end
   end
 
   get '/teas/new' do
-    erb :'teas/create_tea'
+    if logged_in?
+      erb :'teas/create_tea'
+    else
+      redirect to "/login"
+    end
   end
 
   post '/teas/new' do
     @tea = Tea.create(kind: params[:kind], purchase_date: params[:purchase_date], brew_time: params[:brew_time], temperature: params[:temperature], grams: params[:grams], servings: params[:servings], name: params[:name], user_id: current_user.id)
     flash[:message] = "Tea has been added."
-    redirect to '/teas/'
+    redirect to '/teas'
   end
 
   get '/teas/:id' do
-    @tea = Tea.find_by_id(params[:id])
-    erb :'teas/show_tea'
+    if logged_in?
+      @user = current_user
+      @tea = Tea.find_by_id(params[:id])
+      erb :'teas/show_tea'
+    else
+      redirect to "/login"
+    end
+  end
+
+  get '/teas/:id/edit' do
+    if logged_in?
+      @user = current_user
+      @tea = Tea.find_by_id(params[:id])
+      if @tea.user_id == current_user.id
+        erb :'teas/edit_tea'
+      else
+        redirect to "/tea"
+      end
+    else
+      redirect to "/login"
+    end
   end
 end

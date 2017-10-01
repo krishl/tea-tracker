abfa4186b5284fbc2bea848a0cf4051c774678de
github username <krishel.lasam@gmail.com>
2017-10-01 05:03:14 -0400

    add comment to user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  has_secure_password
+  has_secure_password   # validates BCrypt passwords
   has_many :teas
   validates :username, :email, uniqueness: true
   validates :username, :email, presence: true
